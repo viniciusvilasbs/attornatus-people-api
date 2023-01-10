6e8a7fd9c10940f96bc107c47f0c984ad42b0d5a
viniciusvilasbs <vinyvilas@gmail.com>
2023-01-10 16:11:56 -0300

    First commit

---
+++ src/main/java/br/com/attornatus/peopleapi/AttornatusPeopleApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.attornatus.peopleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttornatusPeopleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttornatusPeopleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/attornatus/peopleapi/AttornatusPeopleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.attornatus.peopleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttornatusPeopleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
